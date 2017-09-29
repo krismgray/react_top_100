e41c781e08f28682caeac6b1223f4bfdf1a96d21
Kris Gray <kris.m.gray@gmail.com>
2017-09-29 04:29:54 -0600

    initialized react side, next add form

---
+++ top100/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Top 100 Songs!</div>
+      //create from and render it here.
+      //view all Songs
+      //edit and delete songs
+    );
+  }
+}
+
+export default App;
